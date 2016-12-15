55bcbe5748fb74fa60ad65daacd81f2fc3f7746c
turdusmerula <sebastien.besombes@gmail.com>
2016-12-16 00:04:56 +0100

    test

--- scripts/get-next-release.sh
+++ scripts/get-next-release.sh
@@ -3,7 +3,7 @@
 # Get the next release number of a package on repository
 
 function usage() {
-	echo "Usage: $0 <ppa> <mirror> <package> <version>"
+	echo "Usage: $0 <ppa> <mirror> <package> <version>" >&2
 }
 
 xpl_path=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
@@ -21,3 +21,17 @@ fi
 
 ${xpl_path}/check-ssh.sh $ppa $mirror || exit 1
 
+# ssh -----------------
+ssh ${ppa}@${mirror} <<EOF
+set -e
+
+aptly package search "${package} (${version})" 2>/dev/null
+
+EOF
+# end ssh -------------
+if [ $? -ne 0 ]
+then
+	echo 1
+fi
+
+exit 0
